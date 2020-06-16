4e96a58271c3a182e65a95642a4c128cf2667ca3
arpita <arpita5572@gmail.com>
2020-06-17 03:06:14 +0530

    Image labeling using Cloud Vision API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Image Labeler',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,7 +31,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Image Labeler'),
     );
   }
 }
@@ -50,17 +55,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  File image;
+  var emptyText = '';
+  var isImageLoaded = false;
+  final picker = ImagePicker();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future getImage() async {
+      emptyText = '';
+      final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+      setState(() {
+      image = File(pickedFile.path);
+      isImageLoaded = true;
+      });
+
+      FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
+
+      final ImageLabeler cloudLabeler = FirebaseVision.instance.cloudImageLabeler();
+      final List<ImageLabel> cloudLabels = await cloudLabeler.processImage(visionImage);
+
+      for (ImageLabel label in cloudLabels) {
+        final double confidence = label.confidence;
+        setState(() {
+          emptyText = emptyText + label.text + ' ' + ' ' + confidence.toStringAsFixed(4) + ' \n';
+          print(emptyText);
+        });
+      }
+
+      cloudLabeler.close();
   }
 
   @override
@@ -77,41 +99,48 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Column(
+          children: <Widget>[
+        SizedBox(height: 100.0),
+        isImageLoaded ? Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+              height: 300,
+              child: Image.file(
+                image,
+                fit: BoxFit.cover,
+              ),
+            ))
+            : Container(),
+            SizedBox(height: 10.0),
+            SizedBox(height: 10.0),
+            emptyText == ''
+                ? Text('')
+                : Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    emptyText,
+                  ),
+                ),
+              ),
+            )
+    ]),
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+      );
   }
 }
+
+
+
+
